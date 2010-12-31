4c3dfabcca91b2cfa04f5bf1c891e68ccebade0f
James McCarthy <james2mccarthy@gmail.com>
2010-12-31 00:09:20 +0000

    Removed the controller module as it makes assumptions about the controller setup that can't be relied upon

--- lib/authlogic_facebook_koala.rb
+++ lib/authlogic_facebook_koala.rb
@@ -7,13 +7,9 @@ require 'authlogic_facebook_koala/adapter'
 require 'authlogic_facebook_koala/acts_as_authentic'
 require 'authlogic_facebook_koala/session'
 require 'authlogic_facebook_koala/helper'
-require 'authlogic_facebook_koala/controller'
-
-
 
 if ActiveRecord::Base.respond_to?(:add_acts_as_authentic_module)
   ActiveRecord::Base.send       :include, AuthlogicFacebookKoala::ActsAsAuthentic
-  ActionController::Base.send   :include, AuthlogicFacebookKoala::Controller
   
   Authlogic::Session::Base.send :extend,  AuthlogicFacebookKoala::Config
   Authlogic::Session::Base.send :include, AuthlogicFacebookKoala::Session
